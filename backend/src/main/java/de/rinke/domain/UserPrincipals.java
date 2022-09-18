c82dda10f799e5fd456347b56299c8494dcca016
benny <benny.sonnefeld@gmx.de>
2022-09-18 21:29:42 +0200

    3.17 Spring Security User domain

---
+++ backend/src/main/java/de/rinke/domain/UserPrincipals.java
@@ -0,0 +1,59 @@
+package de.rinke.domain;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+@RequiredArgsConstructor
+public class UserPrincipals implements UserDetails {
+
+    @NonNull
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return stream(this.user.getAuthorities())
+                //.map(SimpleGrantedAuthority::new)
+                .map(auth -> {
+                   return new SimpleGrantedAuthority(auth);
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.user.isNotLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isActive();
+    }
+}
