0eb185a63dd85bbd896df22a1a322b5328cc1dbc
benny <benny.sonnefeld@gmx.de>
2022-10-10 19:33:08 +0200

    4.31 Authentication entry point

---
+++ backend/src/main/java/de/rinke/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package de.rinke.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.rinke.domain.HttpResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static de.rinke.constant.SecurityConstant.FORBIDDEN_MESSAGE;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
+            throws IOException {
+        HttpResponse httpResponse = new HttpResponse(FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(), FORBIDDEN_MESSAGE);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream,httpResponse);
+        outputStream.flush();
+    }
+}
